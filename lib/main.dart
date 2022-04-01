b392ef34dbe681296b4ed92892560d0abc0626b6
Vicranzer <victormaina1962@gmail.com>
2022-04-01 14:32:29 +0300

    jun

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: NinjaCard()));
+}
+class NinjaCard extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(title: Text('Ninja ID Card'),
+      centerTitle: true,
+      backgroundColor: Colors.grey[850],
+      elevation: 0.0,
+    ),
+    body: Padding(
+padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+child: Column(
+  crossAxisAlignment: CrossAxisAlignment.start,
+  children: <Widget>[
+    Center(
+      child: CircleAvatar( backgroundImage: AssetImage('assets/vance_joy.jpg',), radius: 70,
+       ),
+    ),
+    Divider( height: 60, color: Colors.grey[800],),
+  Text(
+    'NAME',
+    style: TextStyle(color: Colors.grey,letterSpacing: 2.0,) 
+    ),
+  
+  SizedBox(height: 10),
+  Text(
+    'The Anxiety of the Ragers',
+    style: TextStyle(color: Colors.amber[200],letterSpacing: 2.0, fontSize: 28,fontWeight: FontWeight.bold ),
+  )
+  ,SizedBox(height: 30),
+  Text(
+    'CURRENT NINJA LEVEL',
+    style: TextStyle(color: Colors.grey,letterSpacing: 2.0 ),
+  ),
+    SizedBox(height: 30),
+  Text(
+    '8',
+    style: TextStyle(color: Colors.amber[200],letterSpacing: 2.0, fontSize: 28,fontWeight: FontWeight.bold ),
+  ),
+SizedBox(height: 30),
+Row(
+children: <Widget>[
+  Icon(Icons.email,color: Colors.red,),
+  SizedBox(width: 30),
+  Text('anxietyragers@gmail.com',style: TextStyle(color: Colors.amber, letterSpacing: 2.0), )
+],
+
+)
+  
+]),
+    ),
+    );
+  }
+}
